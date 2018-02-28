7ca0eb25f819a100a7e5516aeafc9ce547a412b7
Fellipeago <36303739+Fellipeago@users.noreply.github.com>
2018-02-28 19:30:18 -0300

    Update exercicio_02.c

--- 22-02/22-02/exercicio_02.c
+++ 22-02/22-02/exercicio_02.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* */
-
 main( ) {  
-
-	
 	int x, *p;
 	x=10;
-	//para salvar o valor de x no endereço p usa-se o & e para declarar um ponteiro se usa  '*'
+	//para salvar o valor de x no endereÃ§o p usa-se o & e para declarar um ponteiro se usa  '*'
 	p = &x;
 	/*forma errada 
 	*p; x=10; */
