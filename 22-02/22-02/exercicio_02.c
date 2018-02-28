a76c3c896fc199dab485317736cd5687d209059c
Fellipeago <36303739+Fellipeago@users.noreply.github.com>
2018-02-28 19:39:36 -0300

    Update exercicio_02.c

--- 22-02/22-02/exercicio_02.c
+++ 22-02/22-02/exercicio_02.c
@@ -6,6 +6,7 @@ main( ) {
 	//para salvar o valor de x no endereço p usa-se o & e para declarar um ponteiro se usa  '*'
 	p = &x;
 	/*forma errada 
+	abaixo foi salvo a memoria de x onde p esta apontando, provavelmente em um endereço (lixo) que estava em p.
 	*p; x=10; */
 	printf	("%d", *p);
 	return 0;
