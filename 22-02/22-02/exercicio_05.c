37fa972271f5a314eac373b82bc242ab11ff6380
Fellipeago <36303739+Fellipeago@users.noreply.github.com>
2018-02-28 19:43:46 -0300

    Update exercicio_05.c

--- 22-02/22-02/exercicio_05.c
+++ 22-02/22-02/exercicio_05.c
@@ -4,13 +4,13 @@
 /*  5. Crie um programa que:
 a. declare e inicialize um vetor do tipo int;
 b. declare e inicialize um vetor do tipo float;
-c. declare um ponteiro para o vetor int e uma vari·vel inteira;
+c. declare um ponteiro para o vetor int e uma vari√°vel inteira;
 d. declare um ponteiro para o vetor do tipo float;
 e. Imprima os elementos do vetor;
-f. Imprima o endereÁo de cada um dos elementos listados. */
+f. Imprima o endere√ßo de cada um dos elementos listados. */
 
 int main() {
-	//declaramos todas as variaveis e vetores e ponteiros pedidos no exercÌcio
+	//declaramos todas as variaveis e vetores e ponteiros pedidos no exerc√≠cio
 	int *v1, variavel_inteira, vetor[1];
 	float *v2, vetor2[1];
 	//vetor[0]=0;
@@ -19,11 +19,10 @@ int main() {
 	v1=&vetor[0];
 	printf("%d\n", vetor[0]);
 	printf("%.2f\n", vetor2[0]);
-	printf("%d\n", v1);
-	printf("%d\n", v2);
-	printf("%d\n", &v1);
-	printf("%d\n", &v2);
-	printf("%d\n", &variavel_inteira);
+	printf("%p\n", v1);
+	printf("%p\n", v2);
+	printf("%p\n", &v1);
+	printf("%p\n", &v2);
+	printf("%p\n", &variavel_inteira);
 	return 0;
 }
-
